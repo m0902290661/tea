6062291bd52ec3818b64c21715d62ad319850db5
m0902290661 <144592540+m0902290661@users.noreply.github.com>
2023-09-23 19:56:53 +0800

    Add files via upload

---
+++ html 5/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        // 在這裡可以加入驗證用戶名和密碼的邏輯
+        // 例如，您可以將它們與預定義的值進行比較，並導向不同的頁面或顯示錯誤消息。
+
+        if (username === "0902" && password === "290661") {
+            alert("登入成功！");
+            window.location.href = "index.htm";
+            // 在此處導向到其他頁面或執行其他操作
+        } else {
+            alert("用戶名或密碼錯誤。請再試一次。");
+        }
+    });
+});
