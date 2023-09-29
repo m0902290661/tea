7c3247a48ec7ffd4fa39aa6ca916bc170a347a07
m0902290661 <144592540+m0902290661@users.noreply.github.com>
2023-09-29 11:47:58 +0800

    Add files via upload

--- html 5/script.js
+++ html 5/script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (username === "0902" && password === "290661") {
             alert("登入成功！");
-            window.location.href = "index.htm";
+            window.location.href = "home copy 5.htm";
             // 在此處導向到其他頁面或執行其他操作
         } else {
             alert("用戶名或密碼錯誤。請再試一次。");
